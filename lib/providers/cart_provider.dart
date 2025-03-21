6e1b6e4c31abda7d2a9fa22e712cf1a83ded5267
AbigailPedreira <abigailpedreira6@gmail.com>
2025-03-21 16:27:58 -0300

    feat(class 9): add cart notifier provider

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,22 +1,17 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
+part 'cart_provider.g.dart';
 
-//esta classe isnt a provider
-//O estado inicial é um conjunto de produtos (Set<Product>)
-class CartNotifier extends Notifier<Set<Product>> {
-  //valor inicial
+@riverpod
+class CartNotifier extends _$CartNotifier {
   @override
   Set<Product> build() {
-    return const {
-      Product(id: '4', title: 'Red Backpack', price: 14, image: 'assets/products/backpack.png'),
-    };
+    return const {};
   }
 
-  //metodos para atualizar o estado
-  //diciona um produto ao carrinho
   void addProduct(Product product) {
     if (!state.contains(product)) {
-      state = {...state, product}; //Cria um novo conjunto com o novo produto
+      state = {...state, product};
     }
   }
 
@@ -26,7 +21,3 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 }
-
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
